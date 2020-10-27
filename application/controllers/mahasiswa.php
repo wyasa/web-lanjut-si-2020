985a79c60c8b4eb112db4a729df98fd8085ffab1
wyasa <wiyanayasa666@gmail.com>
2020-10-27 12:04:03 +0800

    j

--- application/controllers/mahasiswa.php
+++ application/controllers/mahasiswa.php
@@ -6,6 +6,11 @@
 class mahasiswa extends Ci_Controller
 {
 	
+	public function index ()
+	{
+		$this->load->view('masasiswa/V_index');
+	}
+	
 	public function jurusan ()
 	{
 		 echo 'D3 Sistem Informasi gimana  ';
