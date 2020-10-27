48e2d718ad36edd38bc135452c0cee107726790e
wyasa <wiyanayasa666@gmail.com>
2020-10-27 13:25:38 +0800

    j

--- application/controllers/mahasiswa.php
+++ application/controllers/mahasiswa.php
@@ -6,11 +6,15 @@
 class mahasiswa extends Ci_Controller
 {
 	
-	public function index ()
+	public function index ($jur=0, $nim=0)
 	{
 		$data ['nama']= 'Gede Wiyana yasa';
 		$data ['alamat']= 'gedur';
-		$data ['nim']= '1901050001';
+		$data ['nim']= $nim;
+		$data ['jurusan']=$jur;
+
+
+		
 
 		$this->load->view('mahasiswa/V_index',$data);
 	}
--- application/views/mahasiswa/v_index.php
+++ application/views/mahasiswa/v_index.php
@@ -4,11 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Data Mahasiswa</title>
+    
 </head>
 <body>
     <h2> Data Mahasiswa<h/2>
     <p> Nama : <?php echo $nama; ?> </p>
     <p> Alamat : <?php echo $alamat; ?> </p>
     <p> NIM : <?php echo $nim; ?> </p>
+    <p> jurusan : <?php echo $jurusan; ?> </p>
+    
 </body>
 </html>
